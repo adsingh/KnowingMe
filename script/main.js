e83015c17725264b7e95c9a96e46e7441d2e5b4a
adsingh <ads7194@gmail.com>
2017-07-25 02:10:53 -0400

    In-page links working properly

--- script/main.js
+++ script/main.js
@@ -1,3 +1,25 @@
+function toggleClass($el){
+    // Get the target from the "data-target" attribute
+    var target = $el.dataset.target;
+    var $target = document.getElementById(target);
+
+    // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+    $el.classList.toggle('is-active');
+    $target.classList.toggle('is-active');
+}
+
+function addEvent(arr){
+
+    arr.forEach(function ($el) {
+      $el.addEventListener('click', () => {
+
+        toggleClass($el);
+
+      });
+    });
+
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 
   // Get all "navbar-burger" elements
@@ -7,19 +29,20 @@ document.addEventListener('DOMContentLoaded', function () {
   if ($navbarBurgers.length > 0) {
 
     // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', () => {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
+    addEvent($navbarBurgers);
   }
 
+  var links = document.getElementsByClassName("toggle");
+  console.log(links.length);
+  Array.prototype.forEach.call(links, (link)=>{
+    link.addEventListener('click',()=>{
+        
+        if ($navbarBurgers.length > 0) {
+            $navbarBurgers.forEach($el=>{
+                toggleClass($el);
+            });
+        }
+    });
+  });
 });
+
